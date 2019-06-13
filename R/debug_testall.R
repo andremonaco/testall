d5f340e05b5febe77f35f6c3c124a23c8fe776a3
andrebleier <andrebleier@live.de>
2019-06-13 10:19:14 +0200

    added debugonce roxygen skeleton

--- R/debug_testall.R
+++ R/debug_testall.R
@@ -1,4 +1,23 @@
-debugonce_testall <- function(sug, scenario = NULL) {
+#' debugonce_testall
+#' This function consumes an object of class \code{testall_summary} with the
+#' ultimate goal to debug your function with a suggested specifications
+#' @param sug an object of class \code{testall_summary}
+#' @param scenario an optional numeric value indicating which scenario from sug$tests
+#' will be used for debugging.
+#'
+#' @return Will take you directly into debug mode and will not return anything
+#' @export
+#'
+#' @examples
+#' # NOT RUN:
+#' suggestion <- test_all(mean, list(x = c('asd')))
+#' debugonce_testall(suggestion)
+debugonce_testall <- function(sug, scenario = 1) {
+
+  # check if there are any errors
+  if(dim(sug$tests)[1] == 0) {
+    return(cat('found no errors - nothing to debug'))
+  }
 
   # function name
   fun <- sug$fun
@@ -9,10 +28,6 @@ debugonce_testall <- function(sug, scenario = NULL) {
 
   scens <- sug$tests[, -ncol(sug$tests)]
 
-  if(!is.null(scenario)) {
-    scenario <- 1
-  }
-
   debug_args <- unlist(scens[scenario,], recursive = FALSE)
 
   debugonce(fun = fun, text = paste0('Scenario: ', paste0(paste(names(debug_args),
@@ -21,4 +36,5 @@ debugonce_testall <- function(sug, scenario = NULL) {
                                      collapse = ", "))
             )
   do.call(fun, args = debug_args)
+  return(invisible())
 }
