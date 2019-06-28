2057375270d105361eb6cde8cb1d3f27b7951d15
Jakob Gepp <jakob.gepp@statworx.de>
2019-06-28 16:23:20 +0200

    add scenario number in print

--- R/print.testall_summary.R
+++ R/print.testall_summary.R
@@ -15,7 +15,19 @@ print.testall_summary <- function(x, ...) {
     cat(names(x)[i], "\n")
     cat(paste0(rep(".", options()$width), collapse = ""), "\n")
     cat("\n\n")
-    cat("Debug suggestion:", paste0(unique(x[[i]]), collapse = " | "), "\n\n")
+
+    all_call_value <- strsplit(x[[i]]$call, " = ")
+    all_call <- lapply(all_call_value, "[[", 1)
+    all_value <- lapply(all_call_value, function(y) {paste0(y[-1], collapse = " = ")})
+
+    this_scenario <- unique(mapply(FUN = function(c, v) {
+      # c <- all_call[[1]]
+      # v <- all_value[[1]]
+      min(which(lapply(suggestion$tests, "[[", c) == v))
+    }, c = all_call, v = all_value))
+
+    cat("Debug suggestion:", paste0(unique(x[[i]]), collapse = " | "), "\n")
+    cat("Scenario:", paste0(this_scenario, collapse = " | "), "\n\n")
   }
 
   cat(rep("\t", options()$width/20), "Error Summary:\n")
